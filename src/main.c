4e3218b9992be5e84c5d8f5e5e721db512a52fa6
Ethan Smith <ethansmith.dev@gmail.com>
2023-12-26 17:29:08 -0500

    removed dead code/comments

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 #include "message.h"
 #include "player_manager.h"
-#include <SDL2/SDL.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -10,38 +9,6 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-//int main(int argc, char** argv) {
-//    SDL_Window* window = NULL;
-//    SDL_Surface* screenSurface = NULL;
-//
-//    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-//        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-//    } else {
-//        window = SDL_CreateWindow("SDL Tutorial",
-//                                  SDL_WINDOWPOS_UNDEFINED,
-//                                  SDL_WINDOWPOS_UNDEFINED,
-//                                  SCREEN_WIDTH,
-//                                  SCREEN_HEIGHT,
-//                                  SDL_WINDOW_SHOWN);
-//        if (window == NULL) {
-//            printf("SDL could not create a window! SDL_Error: %s\n",
-//                   SDL_GetError());
-//        } else {
-//            screenSurface = SDL_GetWindowSurface(window);
-//            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format,
-//                                                         0xff, 0xff, 0xff));
-//            SDL_UpdateWindowSurface(window);
-//            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-//
-//            SDL_DestroyWindow(window);
-//            SDL_Quit();
-//        }}
-//
-//    return 0;
-//}
 #include <scenario.h>
 #include <message.h>
 #include <pthread.h>
